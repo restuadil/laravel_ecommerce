429f4787c6ffb3f60532541fc7a5eba4a3d06d67
restuadil <restuadil09@gmail.com>
2024-10-17 13:39:57 +0700

    creating CRUD view for products path

---
+++ resources/views/admin/products/show.blade.php
@@ -0,0 +1,39 @@
+<x-admin-layout>
+    <div class="max-w-2xl mx-auto bg-slate-300 p-8 rounded-lg shadow-lg">
+        <h1 class="text-center font-semibold text-3xl mb-4">Product Detail</h1>
+
+        <div class="mb-4">
+            <strong>Name:</strong>
+            <p>{{ $product->name }}</p>
+        </div>
+
+        <div class="mb-4">
+            <strong>Price:</strong>
+            <p>Rp.{{ $product->price }}</p>
+        </div>
+        <div class="mb-4">
+            <strong>Stock:</strong>
+            <p>{{ $product->stock }}</p>
+        </div>
+
+        <div class="mb-4">
+            <strong>Description:</strong>
+            <p>{{ $product->description }}</p>
+        </div>
+
+        <div class="mb-4">
+            <strong>Category:</strong>
+            <p>{{ $product->category->name }}</p>
+        </div>
+
+
+
+        <div class="flex justify-center">
+            <a href="{{ route('admin.products.index') }}"
+                class="bg-blue-600 text-white px-6 py-2 rounded-md text-lg font-semibold shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                Back to Products
+            </a>
+        </div>
+    </div>
+
+</x-admin-layout>
